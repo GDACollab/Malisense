e506e1f839122871f9d8f6e4e186a97598035740
Jonathan Cheng <chengjonathan2021@gmail.com>
2024-01-19 00:55:25 -0800

    FMOD Studio and More Code

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OptionsMenu()
     {
-        
+
+    }
+
+    public void Credits()
+    {
+
+
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
     }
 }
