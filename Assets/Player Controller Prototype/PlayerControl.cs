4b0cfb3523dcea241056deae0ac788377fdd077d
Justin-Lam <justinflam9225k@gmail.com>
2024-01-19 15:57:56 -0800

    Streamlined Movement and Sprinting Code
    Diagonal movement speed didn't need to be divided by sqrt(2)
    
    Removed unnecessary code, cleaned up code, renamed assets and variables, and added comments

--- Assets/Player Controller Prototype/PlayerControl.cs
+++ Assets/Player Controller Prototype/PlayerControl.cs
@@ -3,62 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-using System; // Math.Sqrt();
 
 public class PlayerControl : MonoBehaviour
 {
-    // Input System
-    PlayerInput playerInput;
-    InputAction moveAction;
-    InputAction sprintAction;
+	// Input System
+	PlayerInput playerInput;
+	InputAction moveAction;
+	InputAction sprintAction;
 
-    [SerializeField] float moveSpeed;
-    [SerializeField] float sprintRatio;
+	// Movement
+	[SerializeField] float moveSpeed;
+	[SerializeField] float sprintRatio;
+	public bool isSprinting;
 
-    public bool isSprinting;
-    float diagonalRatio = (float)Math.Sqrt(1.0 / 2.0);
+	void Start()
+	{
+		// Populate the Input System variables
+		playerInput = GetComponent<PlayerInput>();
+		moveAction = playerInput.actions.FindAction("8 Directions Movement");
+		sprintAction = playerInput.actions.FindAction("Sprint");
+	}
 
-    void Start()
-    {
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions.FindAction("8 Directions Movement");
-        sprintAction = playerInput.actions.FindAction("Sprint");
-    }
+	void Update()
+	{
+		Vector2 newPosition = transform.position;		// where the player should move to
+		float adjustedSpeed = moveSpeed;				// the distance the player moves, adjusted for sprinting
 
-    void Update()
-    {
-        Vector2 currentPosition = transform.position;
-        float adjustedSpeed = moveSpeed;
+		// Player is sprinting?
+		if (sprintAction.ReadValue<float>() > 0f)
+		{
+			adjustedSpeed *= sprintRatio;
+			isSprinting = true;
+		}
+		else
+		{
+			isSprinting = false;
+		}
 
-        // If moving diagonally, decrease the speed based on the Pythagorean Theorem
-        if ((moveAction.ReadValue<Vector2>().y > 0 && (moveAction.ReadValue<Vector2>().x < 0 || moveAction.ReadValue<Vector2>().x > 0)) ||
-            (moveAction.ReadValue<Vector2>().y < 0 && (moveAction.ReadValue<Vector2>().x < 0 || moveAction.ReadValue<Vector2>().x > 0)))
-        {
-            adjustedSpeed *= diagonalRatio;
-        }
+		// Calculate the position the player will move to
+		newPosition += moveAction.ReadValue<Vector2>() * adjustedSpeed * Time.deltaTime;
 
-        if (sprintAction.ReadValue<float>() > 0f)
-        {
-            adjustedSpeed *= sprintRatio;
-            isSprinting = true;
-        }
-        else
-        {
-            isSprinting = false;
-        }
+		// Don't let player move past the boundary
+		newPosition.x = Mathf.Clamp(newPosition.x, -960, 960);
+		newPosition.y = Mathf.Clamp(newPosition.y, -540, 540);
 
-        // Input System
-        float moveX = moveAction.ReadValue<Vector2>().x;
-        float moveY = moveAction.ReadValue<Vector2>().y;
-
-        currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
-        currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
-
-        // Setting up the boundary for the ball
-        currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
-        currentPosition.y = Mathf.Clamp(currentPosition.y, -540, 540);
-
-        // Apply the new position every frame
-        transform.position = currentPosition;
-    }
+		// Apply the new position every frame
+		transform.position = newPosition;
+	}
 }
