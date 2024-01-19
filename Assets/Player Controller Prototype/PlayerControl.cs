bb90f4d70cd12db8962263f9a8a61f48c7a235da
umjunsik <sooin0514@gmail.com>
2024-01-19 14:48:36 -0800

    Modified keyboard sprint input, Added controller 8 direction movement

--- Assets/Player Controller Prototype/PlayerControl.cs
+++ Assets/Player Controller Prototype/PlayerControl.cs
@@ -10,64 +10,49 @@ public class PlayerControl : MonoBehaviour
     // Input System
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction sprintAction;
 
     [SerializeField] float moveSpeed;
-    [SerializeField] float sprintSpeed;
-    float diagonalRatio = (float)Math.Sqrt(1.0 / 2.0);
+    [SerializeField] float sprintRatio;
 
-    private NewControls newControls;
     public bool isSprinting;
+    float diagonalRatio = (float)Math.Sqrt(1.0 / 2.0);
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("8 Directions Movement");
+        sprintAction = playerInput.actions.FindAction("Sprint");
     }
 
-    private void Awake()
-    {
-        newControls = new NewControls();
-
-        newControls.Player.StartSprint.performed += x => SprintPressed();
-        newControls.Player.EndSprint.performed += x => SprintReleased();
-    }
-    private void SprintPressed()
-    {
-        isSprinting = true;
-    }
-    private void SprintReleased()
-    {
-        isSprinting = false;
-    }
     void Update()
     {
         Vector2 currentPosition = transform.position;
         float adjustedSpeed = moveSpeed;
-        float adjustedSprintSpeed = sprintSpeed;
 
         // If moving diagonally, decrease the speed based on the Pythagorean Theorem
         if ((moveAction.ReadValue<Vector2>().y > 0 && (moveAction.ReadValue<Vector2>().x < 0 || moveAction.ReadValue<Vector2>().x > 0)) ||
             (moveAction.ReadValue<Vector2>().y < 0 && (moveAction.ReadValue<Vector2>().x < 0 || moveAction.ReadValue<Vector2>().x > 0)))
         {
             adjustedSpeed *= diagonalRatio;
-            adjustedSprintSpeed *= diagonalRatio;
+        }
+
+        if (sprintAction.ReadValue<float>() > 0f)
+        {
+            adjustedSpeed *= sprintRatio;
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
         }
 
         // Input System
         float moveX = moveAction.ReadValue<Vector2>().x;
         float moveY = moveAction.ReadValue<Vector2>().y;
 
-        if (isSprinting)
-        {
-            currentPosition.x += moveX * adjustedSprintSpeed * Time.deltaTime;
-            currentPosition.y += moveY * adjustedSprintSpeed * Time.deltaTime;
-        }
-        else
-        {
-            currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
-            currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
-        }
-        
+        currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
+        currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
 
         // Setting up the boundary for the ball
         currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
@@ -75,15 +60,5 @@ public class PlayerControl : MonoBehaviour
 
         // Apply the new position every frame
         transform.position = currentPosition;
-    }
-
-    private void OnEnable()
-    {
-        newControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        newControls.Disable();
     }
 }
