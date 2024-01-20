18b5b8f044260a61aae8594e4f028345cb0ecb14
Mason <103815820+Ikey11@users.noreply.github.com>
2024-01-20 00:37:22 -0800

    Added empty chase script
    This script will use the pathfinding data to accelerate towards player.

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Chase : MonoBehaviour
+{
+    // TODO: Transfer pathfinding commands into force commands for more realistic movement
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+
+    }
+}
