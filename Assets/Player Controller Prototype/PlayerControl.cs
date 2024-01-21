7f6dcaa4e0aa6590ad65f7181adf11dff4b2b1c1
John-Sai <johnsai999@gmail.com>
2024-01-20 21:13:50 -0800

    stamina changes/polishing
    -Added a rudimentary stamina bar
    -tweaked movement speed, sprint ratio, stamina drain, and minimum stamina required to sprint
    -added movement check (isMoving bool)
    -made it so you can't sprint if not moving

--- Assets/Player Controller Prototype/PlayerControl.cs
+++ Assets/Player Controller Prototype/PlayerControl.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -10,16 +12,28 @@ public class PlayerControl : MonoBehaviour
     InputAction moveAction;
     InputAction sprintAction;
 
+    //stamina bar stuff, just temporary janky bar to keep track of stamina easier for debug purposes, can change it and make it look nicer later
+    public Image StaminaBar;
+
     [SerializeField] float moveSpeed;
     [SerializeField] float sprintRatio;
+    [SerializeField] float maxStamina; //How many seconds of sprinting
+    [SerializeField] float minimumToSprint; //Determines when player can run again, the variable should be a percentage
+    float currentStamina; //How many seconds of springing left
+    bool exhausted = false; //If character's stamina reaches below 0, give exhausted state to player which makes it so they cannot run 
+    // until stamina regenerates back to a certain threshold.
 
+    public bool isMoving;
     public bool isSprinting;
 
     void Start()
     {
+
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("8 Directions Movement");
         sprintAction = playerInput.actions.FindAction("Sprint");
+        currentStamina = maxStamina;
+
     }
 
     void Update()
@@ -27,20 +41,41 @@ public class PlayerControl : MonoBehaviour
         Vector2 currentPosition = transform.position;
         float adjustedSpeed = moveSpeed;
 
+        //Ensures that currentStamina doesn't go over max Stamina
+        if (currentStamina > maxStamina)
+            currentStamina = maxStamina;
+
+        //Makes character not exhausted anymore
+        if (currentStamina > maxStamina * minimumToSprint)
+            exhausted = false;
+
         // Sprint functionality
-        if (sprintAction.ReadValue<float>() > 0f)
+        if ((sprintAction.ReadValue<float>() > 0f) && (currentStamina > 0f) && (!exhausted) && (isMoving == true)) //Two new test cases (actually added a 3rd, only starts sprinting if moving)
+                                                                                             //2nd check to see if currentStamina greater than 0, then check to see if character is exhausted
         {
+            currentStamina -= 2 * Time.deltaTime; //The rate at which stamina drains (drains 1 every second)
+            if (currentStamina < 0f) //Makes the exhaust variable true, which means the character cannot run for some time.
+                exhausted = true;
+            
             adjustedSpeed *= sprintRatio;
             isSprinting = true;
         }
         else
         {
             isSprinting = false;
+
+            if (currentStamina < maxStamina)
+                currentStamina += 1 * Time.deltaTime; //The rate at which stamina regenerates (Regenerates 1 every second)
+
         }
 
         // Input System
         float moveX = moveAction.ReadValue<Vector2>().x;
         float moveY = moveAction.ReadValue<Vector2>().y;
+
+        // Movement check
+        if (moveX != 0 || moveY != 0) isMoving = true;
+        else isMoving = false;
 
         currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
         currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
@@ -51,5 +86,8 @@ public class PlayerControl : MonoBehaviour
 
         // Apply the new position every frame
         transform.position = currentPosition;
+
+        //stamina bar stuff, just temporary janky bar to keep track of stamina easier for debug purposes, can change it and make it look nicer later
+        StaminaBar.fillAmount = currentStamina / maxStamina;
     }
 }
