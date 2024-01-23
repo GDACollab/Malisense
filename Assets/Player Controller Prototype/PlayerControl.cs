8ee243ffbedc5fbaf58a13baf08adf4738a76659
Justin-Lam <justinflam9225k@gmail.com>
2024-01-23 15:06:32 -0800

    Made Inspector Variables more Designer Friendly

--- Assets/Player Controller Prototype/PlayerControl.cs
+++ Assets/Player Controller Prototype/PlayerControl.cs
@@ -1,108 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Input System
-    PlayerInput playerInput;
-    InputAction moveAction;
-    InputAction sprintAction;
-    InputAction sneakAction;
+	// Input System
+	PlayerInput playerInput;
+	InputAction moveAction;
+	InputAction sprintAction;
+	InputAction sneakAction;
 
-    //stamina bar stuff, just temporary janky bar to keep track of stamina easier for debug purposes, can change it and make it look nicer later
+	// Movement
+	[Header("Movement")]
+	[SerializeField] float moveSpeed;
+	[SerializeField] float sprintRatio;
+	[SerializeField] float sneakRatio;
+	[SerializeField] float maxStamina;
+    [Tooltip("percentage of stamina required to sprint again")] [Range(0.00f, 1.00f)] [SerializeField] float minimumToSprint;
+    [Tooltip("n stamina per second")] [SerializeField] float staminaRegen;
+    [Tooltip("n stamina per second")] [SerializeField] float staminaDepletion;
+	float currentStamina;
+	bool exhausted = false;                 // makes it so player can't run; true when stamina is 0, false when currentStamina >= minimumToSprint
+
+    // Stamina Bar
+    [Header("UI")]
     public Image StaminaBar;
 
-    [SerializeField] float moveSpeed;
-    [SerializeField] float sprintRatio;
-    [SerializeField] float sneakRatio;
-    [SerializeField] float maxStamina; //How many seconds of sprinting
-    [SerializeField] float minimumToSprint; //Determines when player can run again, the variable should be a percentage
-    float currentStamina; //How many seconds of springing left
-    bool exhausted = false; //If character's stamina reaches below 0, give exhausted state to player which makes it so they cannot run 
-    // until stamina regenerates back to a certain threshold.
+    // Movement States
+    [Header("Movement States (for other scripts to use)")]
+	public bool isMoving;
+	public bool isSprinting;
+	public bool isSneaking;
 
-    public bool isMoving;
-    public bool isSprinting;
-    public bool isSneaking;
+	void Start()
+	{
+		// Set Input System Variables
+		playerInput = GetComponent<PlayerInput>();
+		moveAction = playerInput.actions.FindAction("8 Directions Movement");
+		sprintAction = playerInput.actions.FindAction("Sprint");
+		sneakAction = playerInput.actions.FindAction("Sneak");
 
-    void Start()
-    {
+		// Set Initial Stamina
+		currentStamina = maxStamina;
+	}
 
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions.FindAction("8 Directions Movement");
-        sprintAction = playerInput.actions.FindAction("Sprint");
-        sneakAction = playerInput.actions.FindAction("Sneak");
-        currentStamina = maxStamina;
+	void Update()
+	{
+		Vector2 currentPosition = transform.position;
+		float adjustedSpeed = moveSpeed;
 
-    }
+		//Ensures that currentStamina doesn't go over max Stamina
+		if (currentStamina > maxStamina)
+			currentStamina = maxStamina;
 
-    void Update()
-    {
-        Vector2 currentPosition = transform.position;
-        float adjustedSpeed = moveSpeed;
+		//Makes character not exhausted anymore
+		if (currentStamina > maxStamina * minimumToSprint)
+			exhausted = false;
 
-        //Ensures that currentStamina doesn't go over max Stamina
-        if (currentStamina > maxStamina)
-            currentStamina = maxStamina;
+		// Sprint functionality
+		if ((sprintAction.ReadValue<float>() > 0f) && (currentStamina > 0f) && (!exhausted) && (isMoving == true)) //Two new test cases (actually added a 3rd, only starts sprinting if moving)
+																							 //2nd check to see if currentStamina greater than 0, then check to see if character is exhausted
+		{
+			currentStamina -= staminaDepletion * Time.deltaTime; //The rate at which stamina drains (drains 1 every second)
+			if (currentStamina < 0f) //Makes the exhaust variable true, which means the character cannot run for some time.
+				exhausted = true;
+			
+			adjustedSpeed *= sprintRatio;
+			isSprinting = true;
+		}
+		else
+		{
+			isSprinting = false;
 
-        //Makes character not exhausted anymore
-        if (currentStamina > maxStamina * minimumToSprint)
-            exhausted = false;
+			if (currentStamina < maxStamina)
+				currentStamina += staminaRegen * Time.deltaTime; //The rate at which stamina regenerates (Regenerates 1 every second)
 
-        // Sprint functionality
-        if ((sprintAction.ReadValue<float>() > 0f) && (currentStamina > 0f) && (!exhausted) && (isMoving == true)) //Two new test cases (actually added a 3rd, only starts sprinting if moving)
-                                                                                             //2nd check to see if currentStamina greater than 0, then check to see if character is exhausted
-        {
-            currentStamina -= 4 * Time.deltaTime; //The rate at which stamina drains (drains 1 every second)
-            if (currentStamina < 0f) //Makes the exhaust variable true, which means the character cannot run for some time.
-                exhausted = true;
-            
-            adjustedSpeed *= sprintRatio;
-            isSprinting = true;
-        }
-        else
-        {
-            isSprinting = false;
+		}
 
-            if (currentStamina < maxStamina)
-                currentStamina += 1 * Time.deltaTime; //The rate at which stamina regenerates (Regenerates 1 every second)
+		//sneaking
+		if (sneakAction.ReadValue<float>() > 0f && !isSprinting)
+		{
+			adjustedSpeed *= sneakRatio;
+			isSneaking = true;
+		}
+		else
+		{
+			isSneaking = false;
+		}
 
-        }
+		// Input System
+		float moveX = moveAction.ReadValue<Vector2>().x;
+		float moveY = moveAction.ReadValue<Vector2>().y;
 
-        //sneaking
-        if (sneakAction.ReadValue<float>() > 0f && !isSprinting)
-        {
-            adjustedSpeed *= sneakRatio;
-            isSneaking = true;
-        }
-        else
-        {
-            isSneaking = false;
-        }
+		// Movement check
+		if (moveX != 0 || moveY != 0) isMoving = true;
+		else isMoving = false;
 
-        // Input System
-        float moveX = moveAction.ReadValue<Vector2>().x;
-        float moveY = moveAction.ReadValue<Vector2>().y;
+		currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
+		currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
 
-        // Movement check
-        if (moveX != 0 || moveY != 0) isMoving = true;
-        else isMoving = false;
+		// Setting up the boundary for the ball
+		currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
+		currentPosition.y = Mathf.Clamp(currentPosition.y, -540, 540);
 
-        currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
-        currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
+		// Apply the new position every frame
+		transform.position = currentPosition;
 
-        // Setting up the boundary for the ball
-        currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
-        currentPosition.y = Mathf.Clamp(currentPosition.y, -540, 540);
-
-        // Apply the new position every frame
-        transform.position = currentPosition;
-
-        //stamina bar stuff, just temporary janky bar to keep track of stamina easier for debug purposes, can change it and make it look nicer later
-        StaminaBar.fillAmount = currentStamina / maxStamina;
-    }
+		//stamina bar stuff, just temporary janky bar to keep track of stamina easier for debug purposes, can change it and make it look nicer later
+		StaminaBar.fillAmount = currentStamina / maxStamina;
+	}
 }
