6810c497b55e9732980383553a04d77cec20f4a0
Juno-Cheng <chengjonathan2021@gmail.com>
2024-01-23 14:51:44 -0800

    Added Fading for Scene

--- Assets/Scripts/Loading/LoaderCallback.cs
+++ Assets/Scripts/Loading/LoaderCallback.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoaderCallback : MonoBehaviour
 {
     private bool isFirstUpdate = true;
+
+    public Image fadeOutUIImage; // Reference to the UI Image
+    public float fadeSpeed = 0.5f;
 
     private void Update()
     {
@@ -18,6 +22,16 @@ public class LoaderCallback : MonoBehaviour
 
     private IEnumerator LoadingDelay()
     {
+        Color objectColor = fadeOutUIImage.color; //Gets Object Color and Modifies values
+
+        while (fadeOutUIImage.color.a > 0)
+        {
+            objectColor.a -= fadeSpeed * Time.deltaTime;
+            fadeOutUIImage.color = objectColor;
+            yield return null;
+        }
+        fadeOutUIImage.gameObject.SetActive(false);
+
         // Set the delay duration (in seconds)
         float delayDuration = 5.0f;
 
