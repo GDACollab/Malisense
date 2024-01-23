c4b53899dae23b3ad829850292f83db7f7cef28e
Juno-Cheng <chengjonathan2021@gmail.com>
2024-01-23 14:21:40 -0800

    Added Fade for Main Menu/Default Brightness

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,12 +8,13 @@ using System;
 
 public class MainMenu : MonoBehaviour
 {
-
+    public Image fadeOutUIImage; // Reference to the UI Image
+    public float fadeSpeed = 0.5f;
 
     //Functions ======================================================================================
     public void PlayGame()
     {
-        Loader.Load(Loader.Scene.GameScene);
+        StartCoroutine(FadeToBlackAndLoadScene());
     }
 
     public void OnApplicationQuit()
@@ -21,6 +22,26 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    
-    
+    IEnumerator FadeToBlackAndLoadScene()
+    {
+        fadeOutUIImage.gameObject.SetActive(true);
+        yield return StartCoroutine(FadeToBlack());
+        Loader.Load(Loader.Scene.GameScene); // Replace with your scene loading logic
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        fadeOutUIImage.gameObject.SetActive(true); // Activate the image if it's not already
+        Color objectColor = fadeOutUIImage.color;
+        float fadeAmount;
+
+        while (fadeOutUIImage.color.a < 1.5)
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            fadeOutUIImage.color = objectColor;
+            yield return null;
+        }
+    }
+
 }
