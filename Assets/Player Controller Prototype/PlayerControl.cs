a204e5c329f779365052a25fcd1a693097f57589
Shinoyhe <ehyoshin@ucsc.edu>
2024-01-23 15:13:46 -0800

    Made Scuffed changes

--- Assets/Player Controller Prototype/PlayerControl.cs
+++ Assets/Player Controller Prototype/PlayerControl.cs
@@ -9,6 +9,7 @@ public class PlayerControl : MonoBehaviour
     PlayerInput playerInput;
     InputAction moveAction;
     InputAction sprintAction;
+    Rigidbody2D player;
 
     [SerializeField] float moveSpeed;
     [SerializeField] float sprintRatio;
@@ -20,11 +21,12 @@ public class PlayerControl : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("8 Directions Movement");
         sprintAction = playerInput.actions.FindAction("Sprint");
+        player = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 currentPosition = transform.position;
+        // Vector2 currentPosition = transform.position;
         float adjustedSpeed = moveSpeed;
 
         // Sprint functionality
@@ -37,19 +39,22 @@ public class PlayerControl : MonoBehaviour
         {
             isSprinting = false;
         }
+        
+        Vector2 vel = new Vector2(moveAction.ReadValue<Vector2>().x * adjustedSpeed, moveAction.ReadValue<Vector2>().y*adjustedSpeed);
+        player.velocity = vel;
+        
+        // // Input System
+        // float moveX = moveAction.ReadValue<Vector2>().x;
+        // float moveY = moveAction.ReadValue<Vector2>().y;
 
-        // Input System
-        float moveX = moveAction.ReadValue<Vector2>().x;
-        float moveY = moveAction.ReadValue<Vector2>().y;
+        // currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
+        // currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
 
-        currentPosition.x += moveX * adjustedSpeed * Time.deltaTime;
-        currentPosition.y += moveY * adjustedSpeed * Time.deltaTime;
-
-        // Setting up the boundary for the ball
-        currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
-        currentPosition.y = Mathf.Clamp(currentPosition.y, -540, 540);
+        // // Setting up the boundary for the ball
+        // currentPosition.x = Mathf.Clamp(currentPosition.x, -960, 960);
+        // currentPosition.y = Mathf.Clamp(currentPosition.y, -540, 540);
 
         // Apply the new position every frame
-        transform.position = currentPosition;
+        // transform.position = currentPosition;
     }
 }
