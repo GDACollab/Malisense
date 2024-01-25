8d15c3be0fc8022c32b10ecf694c1dd9b4c43731
KingLemurs <xxdjroundtablexx@gmail.com>
2024-01-24 19:59:05 -0800

    ItemPrefab Base Class proto working
    idk

--- Assets/Scripts/ItemObject.cs
+++ Assets/Scripts/ItemObject.cs
@@ -1,8 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemObject : MonoBehaviour
 {
-    public ItemBase item;
+    private GameObject player;
+    private AudioSource speaker;
+    private Rigidbody2D rb;
+    
+    private int lifespan;
+    [SerializeField] 
+    bool isThrowable;
+    
+    [SerializeField, Tooltip("How far the object will get thrown")]
+    float throwStrength = 100;
+    
+    [SerializeField]
+    float effectRadius = 50;
+
+    private void Start()
+    {
+        // ref to player, always good to have
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        speaker = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+
+        // idk if yall want this 
+        if (isThrowable)
+        {
+            rb.velocity = player.transform.up * throwStrength;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
