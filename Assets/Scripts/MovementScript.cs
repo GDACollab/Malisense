656de828053835fc10ead78b3f946b36450b98a7
Thryloz <86812547+Thryloz@users.noreply.github.com>
2024-01-29 15:03:43 -0800

    Revert "cleaned up to prep for merge"
    This reverts commit 2d90aa12a1686c3f73716ea7060c3adbc6eb8466.

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+// MODIFIED FROM https://stuartspixelgames.com/2018/06/24/simple-2d-top-down-movement-unity-c/ but won't be in final code anyway
+public class MovementScript : MonoBehaviour 
+{   
+    Rigidbody2D rb;
+    
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 10;
+    public Canvas floorNote;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+    // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        floorNote.gameObject.SetActive(true);
+        
+    }
+}
