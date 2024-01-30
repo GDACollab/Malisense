531c8ac74ab6f03779af3c2d49d957a48b70235f
Justin-Lam <justinflam9225k@gmail.com>
2024-01-30 15:05:27 -0800

    Finished the task???
    Attatched box collider 2D to Player (New) prefab
    Finished PlayerDeathContactEnemy script
    Set Player (New) prefab tag to "Player" since it should probably be that way; this isn't important to the task though
    Set Enemy (New) prefab tag to "Enemy" this is important to the task because the player can only die if they collide with something with the "Enemy" tag

--- Assets/Sprint 2/Player Death System/PlayerDeathContactEnemy.cs
+++ Assets/Sprint 2/Player Death System/PlayerDeathContactEnemy.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerDeathContactEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Player died due to contact to enemy");
+        }
     }
 }
