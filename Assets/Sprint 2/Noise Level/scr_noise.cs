caa3a4f490bf4b6369ab6011df2581e5fcf4f162
AshtonTankGallistel <Ashtontankgallistel@gmail.com>
2024-01-30 15:02:16 -0800

    Made test scene and created sound machine and scripte
    no new features, just setup for later. all new scripts and objects are currently empty.

---
+++ Assets/Sprint 2/Noise Level/scr_noise.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_noise : MonoBehaviour
+{
+    //In case I'm unable to work on this:
+    //When a character move, it creates a sound circle around it
+    //size of circle is based on movement type
+    //a command from this script gets called, creating the object on top of the character that moved
+    //If the circle hits a sound monster, it gets alerted
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
