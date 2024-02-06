9369425f7f2b7c004fd5a6a1056bb79d97d719d6
ShahVishrut <53956360+ShahVishrut@users.noreply.github.com>
2024-02-05 16:15:45 -0800

    Update EnemyAlert.cs

--- Assets/Sprint 2/Enemy AI Tasks/Enemy AI Search/EnemyAlert.cs
+++ Assets/Sprint 2/Enemy AI Tasks/Enemy AI Search/EnemyAlert.cs
@@ -8,15 +8,19 @@ public class EnemyAlert : StateBaseClass
     // Start is called before the first frame update
     public Transform target;
 
-    public float speed = 200f;              // Acceleration speed
-    public float nextWaypointDistance = 3f; // How close an enemy needs to be to the waypoint to move on to next one
-    public float pathUpdateRate = 0.5f;     // Time between pathfinder updates
+    public float speed = 100f;              // Acceleration speed
+    public float nextWaypointDistance = 1f; // How close an enemy needs to be to the waypoint to move on to next one
+    public float circleRadius = 1f;
 
     public Transform GFX;
 
     Path path;
     int currentWaypoint = 0;
     public bool reachedEndOfPath = false;
+    public bool onCircumference = false;
+    public int circumferenceIndex = 0;
+    public Vector2 center;
+    public Vector2[] circlePoints = { new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, 0), new Vector2(0, 1) };
 
     Seeker seeker;
     Rigidbody2D rb;
@@ -25,45 +29,56 @@ public class EnemyAlert : StateBaseClass
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-        seeker.StartPath(rb.position, target.position);
-        Debug.Log(target.position);
-        Debug.Log(speed);
-
-        // Update pathfinding
-        //InvokeRepeating("UpdatePath",   // Method name
-        //                0f,             // Time to wait before method call
-        //               pathUpdateRate);// UpdatePath delay
+        center = new Vector2(target.position.x,target.position.y);
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
 
-    //void UpdatePath()
-    //{
-    //    if (seeker.IsDone())
-    //        seeker.StartPath(rb.position, target.position, OnPathComplete);
-    //}
 
-    //void OnPathComplete(Path p)
-    //{
-    //    if (!p.error)
-    //    {
-    //        path = p;
-    //        currentWaypoint = 0;    // Start at beginning of new path
-    //    }
-    //}
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;    // Start at beginning of new path
+        }
+    }
 
     public override void On_Update()
     {
+        
         if (path == null)
             return;
+
+
+        if (onCircumference)
+        {
+            Vector2 circleDirection = center + circleRadius * circlePoints[circumferenceIndex] - rb.position;
+            rb.velocity = circleDirection.normalized * speed * Time.deltaTime;
+            if (circleDirection.magnitude < 0.02)
+            {
+                circumferenceIndex = (circumferenceIndex + 1) % circlePoints.Length;
+            }
+            return;
+        }
+
+        if (reachedEndOfPath)
+        {
+            
+            Vector2 upwards = new Vector2(0, 1);
+            rb.velocity = upwards * speed * Time.deltaTime;
+            float centerDistance = Vector2.Distance(rb.position, center);
+            if (centerDistance >= circleRadius || rb.velocity.y <= 0.2)
+            {
+                onCircumference = true;
+            }
+            return;
+        }
 
         // If exceeded current amount of waypoints in the path
         if (currentWaypoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = true;
             return;
-        }
-        else
-        {
-            reachedEndOfPath = false;
         }
 
         // Find direction to waypoint
@@ -79,6 +94,12 @@ public class EnemyAlert : StateBaseClass
             currentWaypoint++;  // Move on to next waypoint
         }
 
+        flipSprite(force);
+        
+    }
+
+    void flipSprite(Vector2 force)
+    {
         // ----- Flip Sprite -----
         // Flip left
         if (force.x >= 0.01f)
