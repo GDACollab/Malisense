09e200cdd041a1a93d3617d13409116e90579e1d
Derek Simpson <articunolies22@gmail.com>
2024-02-08 19:58:43 -0800

    Final Implementation
    -Door controller
    -- Script that can be placed on a object
    --This script takes in the Door Object, and in the inspector you are able to add as much switches as you would like to activate it.
    --Needs sound and animation added to it.
    -Switch Controller
    --Takes in a Lamp Prefab that is activated once the switch is interacted with
    --Needs Sound and Animation
    -Lamp Prefab
    --Copy of Torch object with Lamp Controller added to it
    --Needs new art and animation and sound?
    -Lamp Controller
    --Sets the object's children active if switch is pulled(which is the lighting system the torch has)
    -DerekPlayerController
    --A variant of Josh's player controller that adds the interact functionality to the switches.
    --Needs to be tested merged with josh's implementation

--- Assets/Sprint 2/Dungeon Switches/LampController.cs
+++ Assets/Sprint 2/Dungeon Switches/LampController.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class LampController : MonoBehaviour
 {
-    //Start the lamp turned off
-    void Start()
+    private GameObject lightObject;
+    private GameObject volumetricObject;
+
+    private void Start()
     {
-        gameObject.SetActive(false);
-    }
-    public void TurnOn()
-    {
-        gameObject.SetActive(true);
+        // Find the "Light" and "Volumetric" children objects
+        lightObject = transform.Find("Light").gameObject;
+        volumetricObject = transform.Find("Volumetric").gameObject;
+
+        // Ensure that the lamp is initially turned off
+        TurnOff();
     }
 
+    public void TurnOn()
+    {
+        // Activate the "Light" and "Volumetric" objects
+        lightObject.SetActive(true);
+        volumetricObject.SetActive(true);
+    }
+
+    public void TurnOff()
+    {
+        // Deactivate the "Light" and "Volumetric" objects
+        lightObject.SetActive(false);
+        volumetricObject.SetActive(false);
+    }
 }
--- Assets/Sprint 2/Dungeon Switches/SwitchController.cs
+++ Assets/Sprint 2/Dungeon Switches/SwitchController.cs
@@ -5,20 +5,9 @@ using static UnityEngine.InputSystem.Controls.AxisControl;
 
 public class SwitchController : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Color activatedColor = Color.green;
     public LampController lamp;
     private bool isActivated = false;
 
-    //Compare the Player prefab transform postion to the switch position and if they are close enough,
-    //activate the switch, the switch cannot be turned off
-    void Update()
-    {
-        if ((Vector3.Distance(transform.position, GameObject.Find("Player").transform.position) < 1.5f) && isActivated == false)
-        {
-            ActivateSwitch();
-        }
-    }
 
     // Called when the switch is clicked or activated
     public void ActivateSwitch()
@@ -27,7 +16,6 @@ public class SwitchController : MonoBehaviour
         lamp.TurnOn();
         // Toggle the switch state
         isActivated = true;
-        spriteRenderer.color = activatedColor;
 
         //ADD SOUND EFFECT HERE
         // You can add a visual feedback or animation for switch activation here
