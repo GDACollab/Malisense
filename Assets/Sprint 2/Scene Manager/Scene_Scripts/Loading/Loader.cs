908acfcdbb95f3745587f1e93998a64a24bad69e
Ivy Dudzik <ivydudzik@gmail.com>
2024-02-12 01:47:32 -0800

    Change Loader Script to Use INDEX

--- Assets/Sprint 2/Scene Manager/Scene_Scripts/Loading/Loader.cs
+++ Assets/Sprint 2/Scene Manager/Scene_Scripts/Loading/Loader.cs
@@ -8,11 +8,11 @@ public static class Loader
 {   
     public enum Scene
     {
-        MainMenu,
-        LoadingScene,
-        Dungeon,
-        Village,
-        DeathScene
+        MainMenu = 0, // This refers to the scene in Build Settings at index 0
+        LoadingScene = 1, // This refers to the scene in Build Settings at index 1
+        Village = 2, // This refers to the scene in Build Settings at index 2
+        Dungeon = 3, // This refers to the scene in Build Settings at index 3
+        DeathScene = 4, // This refers to the scene in Build Settings at index 4
     }
 
     private static Action onLoaderCallback;
@@ -21,11 +21,11 @@ public static class Loader
         //Action is stored, then is called from function LoaderCallback
         onLoaderCallback = () =>
         {
-            SceneManager.LoadScene(scene.ToString());
+            SceneManager.LoadScene((int)scene); // USES BUILD SETTINGS INDEX, NOT NAME 
         };
 
         //Load Loading Scene which calls on the function that runs Action
-        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+        SceneManager.LoadScene((int)Scene.LoadingScene);
     }
 
     public static void LoaderCallback()
