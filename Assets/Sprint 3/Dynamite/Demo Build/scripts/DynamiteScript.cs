ee450546831610ad679fdbf92d59a9a3d412d4e3
Thryloz <86812547+Thryloz@users.noreply.github.com>
2024-02-20 00:45:29 -0800

    i made dynamite appear when you left click lol

--- Assets/Sprint 3/Dynamite/Demo Build/scripts/DynamiteScript.cs
+++ Assets/Sprint 3/Dynamite/Demo Build/scripts/DynamiteScript.cs
@@ -1,32 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEditor.Progress;
 
 public class DynamiteScript : MonoBehaviour
 {
     private Camera camera;
 
-    Vector2 playerPosition;
+    [SerializeField] GameObject DynamitePrefab;
+    
+    
+    public GameObject playerPosition;
+    public GameObject thrown_Dynamite;
     Vector2 mousePosition;
-    Vector2 distance;
 
 
     // Start is called before the first frame update
     void Start()
     {
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = transform.position;
+        
         mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("throw dynamite");
+            thrown_Dynamite = Instantiate(DynamitePrefab);
+            thrown_Dynamite.transform.position = playerPosition.transform.position;
+            Debug.Log(mousePosition);
         }
     }
 
