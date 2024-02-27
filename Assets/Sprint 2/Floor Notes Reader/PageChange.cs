6369ab9a8920e5b1891e5a413d9469c4825f3ad7
JustinVFong <justinvf2018@gmail.com>
2024-02-27 14:10:46 -0800

    added a PageChange script
    lets you click the "tabs" to switch pages

--- Assets/Sprint 2/Floor Notes Reader/PageChange.cs
+++ Assets/Sprint 2/Floor Notes Reader/PageChange.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PageChange : MonoBehaviour
 {
 
+    public GameObject[] pages; 
+    public Button[] bookmarks;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        // show the first page
+        ShowPage(0);
+
+        for (int i = 0; i < bookmarks.Length; i++)
+        {
+            int pageIndex = i; 
+            bookmarks[i].onClick.AddListener(() => ShowPage(pageIndex));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ShowPage(int pageIndex) //only enables the selected page
     {
-        
+        //disabling all the pages
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+        }
+
+        //enabling the selected page
+        pages[pageIndex].SetActive(true);
     }
+
 }
