6a978394a4deeb333a479f609ddd40871ec1c27b
ShahVishrut <shahsvishrut@gmail.com>
2024-04-16 15:58:32 -0700

    added more

---
+++ Assets/Vertical Slice/Merge State Machines/AltarScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AltarScript : MonoBehaviour
+{
+    // Remove once alert goes to patrol automatically
+    [Header("Time")]
+    [SerializeField][Tooltip("Time after entering altar until enemies return to patrol")] private float alertCooldown = 5f;
+    
+    GameObject[] enemyObjects;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            foreach(GameObject eObj in enemyObjects){
+                StateMachine enemy = eObj.GetComponent<StateMachine>();
+                // Remove if alert when alert goes to patrol automatically
+                if(enemy.currentState==StateMachine.State.Chasing || enemy.currentState==StateMachine.State.Alert){ 
+                    // enemy.switchState(StateMachine_Updated.State.Alert); 
+                    // Replace with just setting to alert when alert goes to patrol automatically
+                    StartCoroutine(CooldownEnemy(enemy)); 
+                }
+            }
+        }
+    }
+    
+    // Remove once alert goes to patrol automatically
+    private IEnumerator CooldownEnemy(StateMachine enemy){
+        enemy.currentState = StateMachine.State.Alert;
+        
+        yield return new WaitForSeconds(alertCooldown);
+        
+        enemy.currentState = StateMachine.State.Patrolling;
+    }
+}
---
+++ Assets/Vertical Slice/Merge State Machines/SightBeastAnimations.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SightBeastAnimations : MonoBehaviour
+{
+    public StateMachine stateMachine;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool("Alert", stateMachine.currentState == StateMachine.State.Alert);
+        _animator.SetBool("Chasing", stateMachine.currentState == StateMachine.State.Chasing);
+    }
+}
