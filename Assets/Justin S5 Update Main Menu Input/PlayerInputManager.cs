c39fffaa32c5da7317cea63ce614f9b07b364e5e
Justin-Lam <justinflam9225k@gmail.com>
2024-04-18 16:13:09 -0700

    Can read player input now
    Inside the Player Input Manager object, the PlayerInput component calls on the Move() and Select() functions inside the PlayerInputManager script

---
+++ Assets/Justin S5 Update Main Menu Input/PlayerInputManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerInputManager : MonoBehaviour
+{
+
+
+	public void Move(InputAction.CallbackContext context)
+	{
+		// when you press a button, there are 3 phases that happen: started, performed, and canceled
+		// we don't want this function to run 3 times when the select button is pressed, so we check for the performed stage
+		if (context.performed)
+		{
+			Vector2 inputVector = context.ReadValue<Vector2>();
+
+			if (inputVector.y > 0f)
+			{
+				// up
+				Debug.Log("up");
+			}
+			else if (inputVector.y < 0f)
+			{
+				// down
+				Debug.Log("down");
+			}
+			else if (inputVector.x > 0f)
+			{
+				// if slider, right
+				Debug.Log("right");
+			}
+			else if (inputVector.x < 0f)
+			{
+				// if slider, left
+				Debug.Log("left");
+			}
+		}
+	}
+
+	public void Select(InputAction.CallbackContext context)
+	{
+		// when you press a button, there are 3 phases that happen: started, performed, and canceled
+		// we don't want this function to run 3 times when the select button is pressed, so we check for the performed stage
+		if (context.performed)
+		{
+			Debug.Log("Select");
+		}
+	}
+}
