82ab5bdf33c62fa78ae9f2c0ccc6b182c72e260c
Justin-Lam <justinflam9225k@gmail.com>
2024-04-19 21:10:06 -0700

    Final Changes
    Added UI navigation capability for right stick, d-pad, and joystick
    Made the buttons in credits 2 and 2 show highlight when hovered
    Made the volume slider have completely no fill at minimum value and completely filled at maximum value
    Made the brightness slider have barely any fill at minimum value (because if there was absolutely 0 brightness the game would just be black then) and completely filled at maximum value
    Fixed a bug that prevented using both mouse and keyboard to navigate from happening

--- Assets/Justin S5 Update Main Menu Input/MainMenuController.cs
+++ Assets/Justin S5 Update Main Menu Input/MainMenuController.cs
@@ -29,7 +29,7 @@ public class MainMenuController : MonoBehaviour
 		if (context.performed)
 		{
 			// there is no currently selected UI element
-			if (eventSystem.currentSelectedGameObject == null)
+			if (eventSystem.currentSelectedGameObject == null || MouseUsedToChangeScreen())
 			{
 				// to prevent the default selectable from getting selected and then having the event system immediately select something else due to a navigation input
 				// ex. on the main menu, when the S key is pressed, without the following line the start button would get selected and then the S key input would then make the options button selected instead
@@ -54,7 +54,7 @@ public class MainMenuController : MonoBehaviour
 		if (context.performed)
 		{
 			// there is no currently selected UI element
-			if (eventSystem.currentSelectedGameObject == null)
+			if (eventSystem.currentSelectedGameObject == null || MouseUsedToChangeScreen())
 			{
 				SelectCurrentScreensDefaultSelectable();
 			}
@@ -79,7 +79,66 @@ public class MainMenuController : MonoBehaviour
 		}
 	}
 
-	void SetNewCurrentScreen(GameObject buttonGO)
+	bool MouseUsedToChangeScreen()
+	// this function exists to fix a bug that occurs when using both the mouse and keyboard to navigate
+	{
+		string buttonClickedName = eventSystem.currentSelectedGameObject.name;      // the button that was clicked and changed screens
+		string buttonClickedScreenName = eventSystem.currentSelectedGameObject.transform.parent.transform.parent.name;        // needed for the next and back buttons found in the credits screens
+
+		// check if the mouse was used to click on a screen-changing button
+		// we know this is true if the event system's currently selected game object is the button that leads to the screen we're currently on
+		// if the keyboard was used to click on a screen-changing button, then SelectCurrentScreensDefaultSelectable() would have changed the event system's currently selected game object accordingly
+		switch (buttonClickedName)
+		{
+			// for example, this below case means "we are on the options screen and the currently selected game object is the options button therefore if must have been clicked with the mouse"
+			case "Options":
+				if (currentScreen == Screens.OPTIONS) { return true; }
+				break;
+
+			case "Credits":
+				if (currentScreen == Screens.CREDITS1) { return true; }
+				break;
+
+			case "ReturnButton":
+				if (currentScreen == Screens.MAIN_MENU) { return true; }
+				break;
+
+			case "NextCreditButton":
+				if (buttonClickedScreenName == "Credits")
+				{
+					if (currentScreen == Screens.CREDITS2) { return true; }
+					break;
+				}
+				else if (buttonClickedScreenName == "Credits2")
+				{
+					if (currentScreen == Screens.CREDITS3) { return true; }
+					break;
+				}
+				break;
+
+			case "BackCreditButton":
+				if (buttonClickedScreenName == "Credits2")
+				{
+					if (currentScreen == Screens.CREDITS1) { return true; }
+					break;
+				}
+				else if (buttonClickedScreenName == "Credits3")
+				{
+					if (currentScreen == Screens.CREDITS2) { return true; }
+					break;
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		// return false if none of the above special cases are true
+		return false;
+	}
+
+	public void SetNewCurrentScreen(GameObject buttonGO)
+	// note: the buttons were instructed to call this function onclick so this function is called even when the player is using the mouse to navigate
 	{
 		string buttonGOName = buttonGO.name;
 		string buttonGOScreenName = buttonGO.transform.parent.transform.parent.name;		// needed for the next and back buttons found in the credits screens
