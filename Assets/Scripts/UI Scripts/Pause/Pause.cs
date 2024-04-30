03d155a8706354f2ca7ad9e19b456a7b2fe6f2a1
Samuel Zhang <sashzhan@ucsc.edu>
2024-04-30 14:56:23 -0700

    Added pause buttons and options function

--- Assets/Scripts/UI Scripts/Pause/Pause.cs
+++ Assets/Scripts/UI Scripts/Pause/Pause.cs
@@ -8,6 +8,7 @@ public class PauseManager : MonoBehaviour
 {
     private static PauseManager instance;
     public Canvas pauseCanvas;
+    [SerializeField] GameObject optionsMenu;
     public static bool isPaused;
     PlayerInput playerInput;
     InputAction pauseButton;
@@ -51,6 +52,13 @@ public class PauseManager : MonoBehaviour
         pauseCanvas.enabled = true;
         Time.timeScale = 0f;
         isPaused = true;
+    }
+
+    public void Options()
+    {
+        pauseCanvas.enabled = false;
+        optionsMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void ResumeGame()
