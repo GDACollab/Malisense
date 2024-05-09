3e8d576d647d7b56d9d5dc6650f42087d0cbe50e
Martin Uribe Hernandez <muribehe@ucsc.edu>
2024-05-09 16:42:48 -0700

    Same issue as before but now with cleaner code

--- Assets/Scripts/UI Scripts/Loading/LoaderCallback.cs
+++ Assets/Scripts/UI Scripts/Loading/LoaderCallback.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoaderCallback : MonoBehaviour
@@ -10,14 +11,12 @@ public class LoaderCallback : MonoBehaviour
 
     public Image fadeOutUIImage; // Reference to the UI Image
     public float fadeSpeed = 0.1f;
-    private Color objectColor;
     private void Update()
     {
         if (isFirstUpdate)
         {
             isFirstUpdate = false;
             StartCoroutine(LoadingDelay());
-            objectColor = fadeOutUIImage.color;
         }
     }
 
@@ -25,24 +24,24 @@ public class LoaderCallback : MonoBehaviour
     {
         //Gets Object Color and Modifies values
 
-        objectColor = fadeOutUIImage.color; //Gets Object Color and Modifies values
-
 
         yield return StartCoroutine(FadeToBlackToLoadScene());
 
-        // Set the delay duration (in seconds)
-        float delayDuration = 0.1f;
+        Loader.LoaderCallback();
 
-        // Wait for the specified duration
-        yield return new WaitForSeconds(delayDuration);
-
+        while (!SceneManager.GetActiveScene().isLoaded)
+        {
+            yield return null;
+        }
+        Debug.Log("Moved here");
         yield return StartCoroutine(FadeToBlackToNextScene());
 
         // Call the loader callback after the delay
-        Loader.LoaderCallback();
+
     }
     private IEnumerator FadeToBlackToLoadScene()
     {
+        Color objectColor = fadeOutUIImage.color; //Gets Object Color and Modifies values
         while (fadeOutUIImage.color.a > 0)
         {
             objectColor.a -= fadeSpeed * Time.deltaTime;
@@ -54,13 +53,15 @@ public class LoaderCallback : MonoBehaviour
 
     private IEnumerator FadeToBlackToNextScene()
     {
+        Color objectColor = fadeOutUIImage.color; //Gets Object Color and Modifies values
         fadeOutUIImage.gameObject.SetActive(true);
-        while (fadeOutUIImage.color.a < 1.5)
+        while (fadeOutUIImage.color.a < 1)
         {
             objectColor.a += fadeSpeed * Time.deltaTime;
             fadeOutUIImage.color = objectColor;
             yield return null;
         }
+        fadeOutUIImage.gameObject.SetActive(false);
     }
 
 }
