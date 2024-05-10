742a6ed3922cac4be58d3b174275f4ca89c3aa9f
Jonah Ryan <jonahrobot@gmail.com>
2024-05-09 19:28:19 -0700

    Sight beast catches player
    Added adjustable acceleration to the chase state of the sight beast. Along with a max velocity.
    
    The new chase logic goes 2 units Infront of player to allow it to catch the player.

--- Assets/Scripts/Enemy Scripts/Sight Beast/SightBeastChase.cs
+++ Assets/Scripts/Enemy Scripts/Sight Beast/SightBeastChase.cs
@@ -9,6 +9,9 @@ public class SightBeastChase : StateBaseClass
     [Tooltip("Rate of acceleration.")]
     public float speed = 200f;
 
+    public float rateOfAcceleration = 1f;
+    public float speedMax = 200f;
+
     [Tooltip("Amount of time that the sight beast will be able to track the player once visual contact is broken, measured in seconds.")]
     public float seeAroundWallsTime = 0.75f;
 
@@ -37,6 +40,10 @@ public class SightBeastChase : StateBaseClass
 
     public override void On_Update()
     {
+        speed = speed * (1 + (rateOfAcceleration * Time.deltaTime));
+        speed = Mathf.Min(speed, speedMax);
+        _pathfinder.acceleration = speed;
+
         var seesTarget = _sight.CanSeeTarget();
         // Target not visable
         if (seesTarget)
@@ -54,7 +61,11 @@ public class SightBeastChase : StateBaseClass
         if (seesTarget || _lastSeenTime + seeAroundWallsTime > Time.time)
         {
             // Move towards target
-            _pathfinder.SetTarget(_sight.target.position);
+
+            // Get point 10 units in front of player
+            Vector3 dir = _sight.target.position - transform.position;
+
+            _pathfinder.SetTarget(_sight.target.position + dir.normalized * 2);
 
             // Look towards target
             //_sight.LookAt(_sight.target.position);
