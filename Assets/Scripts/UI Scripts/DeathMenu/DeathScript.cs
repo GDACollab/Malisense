97e87e9d86c53888918b44c4b1bb6071e692eec0
tristynl <tristyn-lai@outlook.com>
2024-05-17 03:11:45 -0700

    Added Game Over Assets

--- Assets/Scripts/UI Scripts/DeathMenu/DeathScript.cs
+++ Assets/Scripts/UI Scripts/DeathMenu/DeathScript.cs
@@ -11,6 +11,10 @@ public class DeathScript : MonoBehaviour
     public Image fadeOutUIImage; // Reference to the UI Image
     public float fadeSpeed = 0.5f;
 
+    //Start of Tristyn's changes
+    //public Text titleText;
+    public Button ariseButton;
+
     //Functions ======================================================================================
     public void ResetButton()
     {
@@ -19,6 +23,10 @@ public class DeathScript : MonoBehaviour
 
     IEnumerator Fader()
     {
+        //
+        //titleText.gameObject.SetActive(false);
+        ariseButton.gameObject.SetActive(false);
+        //
         fadeOutUIImage.gameObject.SetActive(true);
         //Play Audio with FMOD - Need to research Library
 
@@ -32,7 +40,7 @@ public class DeathScript : MonoBehaviour
         Color objectColor = fadeOutUIImage.color;
         float fadeAmount;
 
-        while (fadeOutUIImage.color.a < 1.5)
+        while (fadeOutUIImage.color.a < 1.0)//1.5
         {
             fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
             objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
