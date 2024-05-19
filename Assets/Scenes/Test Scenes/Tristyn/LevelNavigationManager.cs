2626d519574918d54ad0df50193d87d14c59493d
AshtonTankGallistel <ashtontankgallistel@gmail.com>
2024-05-18 23:31:53 -0700

    Got it working!
    Mean to push this yesterday, sorry. The player is now able to choose between the 2 doors in the level select menu, and can enter the dungeon through them, with each respective door leading to their respective dungeon. However, there is currently no check in place to make sure that the player has actually unlocked he respective dungeon before entering.

--- Assets/Scenes/Test Scenes/Tristyn/LevelNavigationManager.cs
+++ Assets/Scenes/Test Scenes/Tristyn/LevelNavigationManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class LevelDoor
 {
     public string levelNumber;
     public GameObject light;
+    public string levelSceneName;
 }
 
 public class LevelNavigationManager : MonoBehaviour
@@ -16,7 +18,7 @@ public class LevelNavigationManager : MonoBehaviour
     enum LevelDoors { ONE, TWO }
     [Header("Door Selection")]      // Looked at Justin Lam's VillageNavigationManager.cs
     [Tooltip("0: LevelOne, 1: LevelTwo")]
-    [SerializeField] LevelDoor[] doors = new LevelDoor[2];        // we know that there's only going to be 5 buildings, so we can use an array
+    [SerializeField] LevelDoor[] doors = new LevelDoor[2];        // we know that there's only going to be 2 doors, so we can use an array
     [Tooltip("Determines which door is selected at the start of the scene.")]
     [SerializeField] LevelDoors initialSelectedDoor;
     public int selectedDoorIndex;
@@ -25,7 +27,7 @@ public class LevelNavigationManager : MonoBehaviour
     //Each "Buildings/NPC"
     [Header("Selection States")]
     [SerializeField] public bool currentlySelected = false;
-    //[SerializeField] public bool hasEntered = false;
+    [SerializeField] public bool hasEntered = false; //was commented out
     [SerializeField] private bool hasSelected = false;
     [SerializeField] private bool movePointer = true;
 
@@ -49,13 +51,9 @@ public class LevelNavigationManager : MonoBehaviour
         // Set selectedDoorIndex to the index of the door that's initially selected at the start of the scene
         selectedDoorIndex = (int)initialSelectedDoor;
 
-        // Set selectedDoor to the door associated with selectedDoorIndex
-        selectedDoor = doors[selectedDoorIndex];
-
-        // Turn on the light of the selected building 
-        doors[selectedDoorIndex].light.SetActive(true);
-
-        thisObject = gameObject;
+        //Update currently selected door based on current index
+        //(Replaced lines previously here with function containing said lines)
+        updateDoor();
         
     }
 
@@ -63,5 +61,74 @@ public class LevelNavigationManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void moveInList(int change)
+    {
+        if (selectedDoorIndex + change < 0 || selectedDoorIndex + change >= doors.Length)
+            return;
+        doors[selectedDoorIndex].light.SetActive(false);
+        selectedDoorIndex += change;
+        updateDoor();
+    }
+
+    //Updates currently selected door based on current index
+    void updateDoor()
+    {
+        //Below lines written by Tristyn. TY Tristyn!
+        // Set selectedDoor to the door associated with selectedDoorIndex
+        selectedDoor = doors[selectedDoorIndex];
+
+        // Turn on the light of the selected building 
+        doors[selectedDoorIndex].light.SetActive(true);
+
+        thisObject = gameObject;
+    }
+
+    public void selectBuilding()
+    {
+        //uh oh! idk what to do here! Im going to return when I know what to do, and hopefully won't completely forget!
+
+        //debug check taken from VilllageNavigationManager.cs
+        if (selectedDoor == null)
+        {
+            Debug.LogError("No selected GameObject.");
+            return;
+        }
+
+        //SceneManager.LoadScene(doors[selectedDoorIndex].levelSceneName);
+        hasEntered = true;
+        StartCoroutine(FadeToBlack());
+    }
+
+    //function taken from VilllageNavigationManager.cs, modified
+    private IEnumerator FadeToBlack() //Coroutine Function
+    {
+        print("FadingToBlack");
+        Color objectColor = fadeOutUIImage.color;
+        float fadeAmount;
+        while (fadeOutUIImage.color.a < 1)
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            fadeOutUIImage.color = objectColor;
+            yield return null;
+        }
+
+        loadDungeon = false;
+        switch (selectedDoorIndex)
+        {
+            case 0:
+                Loader.Load(Loader.Scene.Dungeon_F1);
+                break;
+            case 1:
+                Loader.Load(Loader.Scene.Dungeon_F2);
+                break;
+            default: //defaults to floor 1 if something went wrong
+                Loader.Load(Loader.Scene.Dungeon_F1);
+                break;
+        }
+
+        //StartCoroutine(FadeToClear()); //FadeToClear not needed as no option would result in diaogue, only scene changes
     }
 }
