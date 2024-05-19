e21a5ee4e8efad27c40924032ed1468bde114fc9
Mason <maysgaming00@gmail.com>
2024-05-18 22:36:56 -0700

    Feed parameters to animator

--- Assets/Scripts/Enemy Scripts/Sight Beast/SightBeastAnimations.cs
+++ Assets/Scripts/Enemy Scripts/Sight Beast/SightBeastAnimations.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class SightBeastAnimations : MonoBehaviour
 {
-    public StateMachine stateMachine;
+    public StateMachine _stateMachine;
 
     private Animator _animator;
+    private Rigidbody2D _rd2d
 
     // Start is called before the first frame update
     void Start()
     {
+        _stateMachine = GetComponent<StateMachine>();
         _animator = GetComponent<Animator>();
+        _rd2d = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _animator.SetBool("Alert", stateMachine.currentState == StateMachine.State.Alert);
-        _animator.SetBool("Chasing", stateMachine.currentState == StateMachine.State.Chasing);
+        // Visual response to startling events
+        _animator.SetBool("Alert", _stateMachine.currentState == StateMachine.State.Alert);
+        _animator.SetBool("Alert", _stateMachine.currentState == StateMachine.State.Distracted);
+
+        // Feed speed data to animator
+        _animator.SetFloat("Speed", _rd2d.velocity.magnitude);
     }
 }
